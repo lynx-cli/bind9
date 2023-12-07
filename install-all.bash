374d5c0cf14a2bc3fc236ab2f8ce2a81e0428dac
lynx-cli <150216440+lynx-cli@users.noreply.github.com>
2023-12-07 15:29:54 +0600

    Update install-all.bash

--- install-all.bash
+++ install-all.bash
@@ -8,15 +8,6 @@ fi
 apt update
 sudo apt install bind9 bind9utils bind9-doc
 
-cat <<EOL > /etc/default/named
-#
-# run resolvconf?
-RESOLVCONF=no
-
-# startup options for the server
-OPTIONS="-u bind"
-EOL
-
 wget -O/usr/share/dns/root.hints https://www.internic.net/domain/named.root
 
 systemctl start bind9
