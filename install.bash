8a2826dcd3c22a90e6b53ece5d4a77da32e08f3d
lynx-cli <150216440+lynx-cli@users.noreply.github.com>
2023-12-07 16:30:35 +0600

    Update install.bash

--- install.bash
+++ install.bash
@@ -6,7 +6,9 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 apt update
-sudo apt install bind9 bind9utils bind9-doc
+apt install bind9 bind9utils bind9-doc
+
+wget -O/usr/share/dns/root.hints https://www.internic.net/domain/named.root
 
 cat <<EOL > /etc/default/named
 #
@@ -17,8 +19,5 @@ RESOLVCONF=no
 OPTIONS="-u bind -4"
 EOL
 
-wget -O/usr/share/dns/root.hints https://www.internic.net/domain/named.root
-
-systemctl start bind9
-systemctl enable bind9
-systemctl status bind9
+systemctl restart named
+systemctl status named
